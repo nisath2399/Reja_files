4c5e9c7b953f16463b7c6a539a2a6f8ea6ffe42d
Munisa <munisaqadamova95@gmail.com>
2025-12-24 11:37:25 +0500

    project

---
+++ server.js
@@ -0,0 +1,38 @@
+console.log("Web serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
+
+//1 expressga kirib kevotgan ma`lumotlarni yozamiz
+//1: Kirish code
+app.use = express(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2: Session code
+
+//3 back-end da view yasimiz va uni clientga jonatamiz
+//3: Views code
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4 routing code
+// app.get("/", function(req, res) {
+//     // res.end(`<h1 style="background: blue">Hello world</h1>`);
+//     res.end("Hello world");
+//     });
+
+// app.get("/", function(req, res) {
+//     res.end("<h1>Siz sovg`alar bo`limidasiz</h1>");
+//     });
+
+app.get("/", function() {
+    res.render("harid")
+});
+
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function() {
+    console.log(`The server is running on port: ${PORT}`);
+});
---
+++ train.js
@@ -0,0 +1,1 @@
+console.log("train rask ishga tushdi");
