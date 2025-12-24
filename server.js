2060e1841346402b753b0c7d163747215561a011
Munisa <munisaqadamova95@gmail.com>
2025-12-24 16:08:47 +0500

    proyekt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const http = require("http");
 
 //1 expressga kirib kevotgan ma`lumotlarni yozamiz
 //1: Kirish code
-app.use = express(express.static("public"));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,8 +25,10 @@ app.set("view engine", "ejs");
 // app.get("/", function(req, res) {
 //     res.end("<h1>Siz sovg`alar bo`limidasiz</h1>");
 //     });
-
-app.get("/", function() {
+app.post("/create-item", (req, res) =>{
+    console.log(req.body);
+});
+app.get("/", function(req, res) {
     res.render("harid")
 });
 
