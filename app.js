06b4cf234019d1f55967b1684d76e5fb2a6a7e6e
Munisa <munisaqadamova95@gmail.com>
2026-01-02 14:15:46 +0500

    Added new functions

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 
 //Mongodb call
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 //1 expressga kirib kevotgan ma`lumotlarni yozamiz
 //1: Kirish code
@@ -29,6 +30,16 @@ app.post("/create-item", (req, res) =>{
         console.log(data.ops);
         res.json(data.ops[0]);
     });
+});
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans")
+    .deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
+        res.json({state: "success"});
+        }
+    );
+    
 });
 
 app.get("/", (req, res) => {
--- public/browser.js
+++ public/browser.js
@@ -1,13 +1,13 @@
 console.log("Salom");
 
-function itemTemplate(data) {
+function itemTemplate(item) {
     return `<li
         class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
         <span class="item-text">${item.reja}></span>
       <div>
         <button 
         data-id="${item._id}>" 
-        class="edit-me btn btn-secodnary btn-sm-mr-1"
+        class="edit-me btn btn-secodnary btn-sm mr-1"
         >
           Ozgartirish
         </button>
@@ -27,7 +27,8 @@ document.getElementById("create-form")
     axios
     .post("/create-item", {reja: createField.value })
     .then((response) => {
-        document.getElementById("item-list")
+        document
+        .getElementById("item-list")
         .insertAdjacentHTML("beforeend", itemTemplate(response.data))
         createField.value ="";
         createField.focus();
@@ -36,3 +37,29 @@ document.getElementById("create-form")
         console.log("Iltimos qaytadan harakat qiling!")
     });
 });
+
+document.addEventListener("click", function(e) {
+  console.log(e.target);
+
+  // delete oper
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirmoqchimisz?")) {
+      axios.post("/delete-item", {
+        id: e.target.getAttribute("data-id")
+      })
+      .then((response) => {
+        console.log(response.data);
+        e.target.parentElement.parentElement.remove();
+      })
+      .catch((err) => {
+        console.log("Iltimos qaytadan harakat qiling!");
+      });
+    }
+  }
+
+  // edit oper
+  if (e.target.classList.contains("edit-me")) {
+    alert("Siz edit tugmasini bosdingiz");
+  }
+});
+
