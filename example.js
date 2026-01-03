918f9a8afc4ccd385747ca2ff3afa39b7547f47d
Munisa <munisaqadamova95@gmail.com>
2026-01-03 23:04:05 +0500

    solved task-c

---
+++ example.js
@@ -0,0 +1,133 @@
+/**********************************
+ * TASK – A
+ * 2 parametrli function
+ * Harfni so‘z ichidan sanaydi
+ **********************************/
+
+// A1
+// countLetter("a", "banana") ➜ 3
+function countLetterA1(letter, word) {
+    let count = 0;
+    for 
+    // Yechimingiz
+}
+
+// A2
+// countLetter("o", "school") ➜ 2
+function countLetterA2(letter, word) {
+    // Yechimingiz
+}
+
+// A3
+// countLetter("m", "programming") ➜ 2
+function countLetterA3(letter, word) {
+    // Yechimingiz
+}
+
+// A4
+// countLetter("i", "mississippi") ➜ 4
+function countLetterA4(letter, word) {
+    // Yechimingiz
+}
+
+// A5
+// countLetter("z", "javascript") ➜ 0
+function countLetterA5(letter, word) {
+    // Yechimingiz
+}
+
+
+
+/**********************************
+ * TASK – B
+ * String ichidagi raqamlarni sanaydi
+ **********************************/
+
+// B1
+// countDigits("abc123") ➜ 3
+function countDigitsB1(str) {
+    // Yechimingiz
+}
+
+// B2
+// countDigits("a1b2c3d4") ➜ 4
+function countDigitsB2(str) {
+    // Yechimingiz
+}
+
+// B3
+// countDigits("noDigitsHere") ➜ 0
+function countDigitsB3(str) {
+    // Yechimingiz
+}
+
+// B4
+// countDigits("2025year") ➜ 4
+function countDigitsB4(str) {
+    // Yechimingiz
+}
+
+// B5
+// countDigits("1a2b3c4d5") ➜ 5
+function countDigitsB5(str) {
+    // Yechimingiz
+}
+
+
+
+/**********************************
+ * TASK – C
+ * Shop class
+ **********************************/
+
+class Shop {
+    constructor(non, lagmon, cola) {
+        this.non = non;
+        this.lagmon = lagmon;
+        this.cola = cola;
+    }
+
+    qoldiq() {
+        // console.log("Hozir ... da ...");
+    }
+
+    sotish(mahsulot, son) {
+        // Yechimingiz
+    }
+
+    qabul(mahsulot, son) {
+        // Yechimingiz
+    }
+}
+
+
+
+/**********************************
+ * TEST QISMI
+ * Shu joyda tekshirasan
+ **********************************/
+
+// C1
+// const shop1 = new Shop(10, 5, 8);
+// shop1.qoldiq();
+
+// C2
+// const shop2 = new Shop(6, 4, 3);
+// shop2.sotish("non", 2);
+// shop2.qoldiq();
+
+// C3
+// const shop3 = new Shop(2, 7, 1);
+// shop3.qabul("cola", 5);
+// shop3.qoldiq();
+
+// C4
+// const shop4 = new Shop(9, 9, 9);
+// shop4.sotish("lagmon", 4);
+// shop4.qabul("non", 3);
+// shop4.qoldiq();
+
+// C5
+// const shop5 = new Shop(1, 1, 1);
+// shop5.sotish("cola", 1);
+// shop5.qoldiq();
--- train.js
+++ train.js
@@ -47,95 +47,137 @@
 
 // console.log("Passed here 1");
 
-// Async / Promise usuli
-// async function maslahatBering(a, callback) {
-//   if (typeof a !== "number") throw new Error("insert a number");
-//   else if (a <= 20) return(list[0]);
-//   else if (a > 20 && a <= 30) return (list[1]);
-//   else if (a > 30 && a <= 40) return (list[2]);
-//   else if (a > 40 && a <= 50) return (list[3]);
-//   else if (a > 50 && a <= 60) return (list[4]);
-//   else {
-//     return new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(list[5]);
-//       }, 5000);
-//     });
-//   };
-//     //   callback(null, list[5]);
-//     // }, 5000);
-//   }
-// // foydalanish usullari:
-// // callback
-// // maslahatBering(65, (err, data) => {
-// //   if (err) {
-// //     console.log("ERROR:", err);
-// //   } else {
-// //     console.log("Maslahat:", data);
+// // Async / Promise usuli
+// // async function maslahatBering(a, callback) {
+// //   if (typeof a !== "number") throw new Error("insert a number");
+// //   else if (a <= 20) return(list[0]);
+// //   else if (a > 20 && a <= 30) return (list[1]);
+// //   else if (a > 30 && a <= 40) return (list[2]);
+// //   else if (a > 40 && a <= 50) return (list[3]);
+// //   else if (a > 50 && a <= 60) return (list[4]);
+// //   else {
+// //     return new Promise((resolve, reject) => {
+// //       setTimeout(() => {
+// //         resolve(list[5]);
+// //       }, 5000);
+// //     });
+// //   };
+// //     //   callback(null, list[5]);
+// //     // }, 5000);
 // //   }
-// // });
-// // console.log("Passed here");
-// //then, chatch
-// // console.log("Passed here");
-// // maslahatBering(65) 
-// //   .then((data) => {
-// //     console.log("Maslahat:", data);
-// //   })
-// //   .catch((err) => {
-// //     console.log("ERROR:", err);
-// //   });
-// // console.log("Passed here 1");
+// // // foydalanish usullari:
+// // // callback
+// // // maslahatBering(65, (err, data) => {
+// // //   if (err) {
+// // //     console.log("ERROR:", err);
+// // //   } else {
+// // //     console.log("Maslahat:", data);
+// // //   }
+// // // });
+// // // console.log("Passed here");
+// // //then, chatch
+// // // console.log("Passed here");
+// // // maslahatBering(65) 
+// // //   .then((data) => {
+// // //     console.log("Maslahat:", data);
+// // //   })
+// // //   .catch((err) => {
+// // //     console.log("ERROR:", err);
+// // //   });
+// // // console.log("Passed here 1");
 
-// async function run() {
-//   let javob = await maslahatBering(65);
-//   console.log("Maslahat:", javob);
-//   let javob2 = await maslahatBering(25);
-//   console.log("Maslahat:", javob2);
-//   let javob3 = await maslahatBering(45);
-//   console.log("Maslahat:", javob3);  
-//   }
-// run();
+// // async function run() {
+// //   let javob = await maslahatBering(65);
+// //   console.log("Maslahat:", javob);
+// //   let javob2 = await maslahatBering(25);
+// //   console.log("Maslahat:", javob2);
+// //   let javob3 = await maslahatBering(45);
+// //   console.log("Maslahat:", javob3);  
+// //   }
+// // run();
 
-// function countDigits(a) {
-//   let count = 0;
-//   for (let i = 0; i < a.length; i++) {
-//     if (a[i] >= '0' && a[i] <= '9') {
-//       count++;
-//     }
+// // function countDigits(a) {
+// //   let count = 0;
+// //   for (let i = 0; i < a.length; i++) {
+// //     if (a[i] >= '0' && a[i] <= '9') {
+// //       count++;
+// //     }
+// //   }
+// //   return count;
+// // }
+// // console.log(countDigits("ad2a54y79wet0sfgb9"));
+
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
 //   }
-//   return count;
+
+//   qoldiq() {
+//     console.log(
+//       "Dokonda " +
+//       this.non + " ta non, " +
+//       this.lagmon + " ta lag'mon va " +
+//       this.cola + " ta cola bor"
+//     );
+//   }
+
+//   sotish(nomi, soni) {
+//     this[nomi] = this[nomi] - soni;
+//     console.log(soni + " ta " + nomi + " sotildi");
+//   }
+
+//   qabul(nomi, soni) {
+//     this[nomi] = this[nomi] + soni;
+//     console.log(soni + " ta " + nomi + " qabul qilindi");
+//   }
 // }
-// console.log(countDigits("ad2a54y79wet0sfgb9"));
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq();
+// shop.sotish("non", 3);
+// shop.qabul("cola", 4);
+// shop.qoldiq();
 
-  qoldiq() {
-    console.log(
-      "Dokonda " +
-      this.non + " ta non, " +
-      this.lagmon + " ta lag'mon va " +
-      this.cola + " ta cola bor"
-    );
-  }
+// Task-c 
 
-  sotish(nomi, soni) {
-    this[nomi] = this[nomi] - soni;
-    console.log(soni + " ta " + nomi + " sotildi");
-  }
+// 1 version
 
-  qabul(nomi, soni) {
-    this[nomi] = this[nomi] + soni;
-    console.log(soni + " ta " + nomi + " qabul qilindi");
-  }
+function countContent(word1, word2) {
+    if (word1.length !== word2.length) {
+        return false;
+    }
+
+    let a = word1.split("").sort().join("")
+    let b = word2.split("").sort().join("")
+
+        return a === b;
+}
+console.log(countContent("mitgroup", "gmtiprou"));
+
+//2-version
+
+function countContent(word1, word2) {
+    if (word1.length !== word2.length) {
+        return false;
+    }
+    function helper(word, letter) {
+        let count = 0;
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] === letter) {
+                count++;
+            }
+        }
+        return count;
+    }
+    for (let i = 0; i < word1.length; i++) {
+        if (helper(word1, word1[i]) !== helper(word2, word1[i])) {
+            return false;
+        }
+    }
+    return true;
 }
 
-const shop = new Shop(4, 5, 2);
-shop.qoldiq();
-shop.sotish("non", 3);
-shop.qabul("cola", 4);
-shop.qoldiq();
+console.log(countContent("mitgroup", "gmtiprou"));
+
