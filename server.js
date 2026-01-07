6d1ce53b6873d91dac86c25a9742526fbe667f08
Kadamova Munisakhon <167546946+nisath2399@users.noreply.github.com>
2026-01-07 06:01:30 +0000

    changed port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongodb.connect(connectionString, {
             module.exports = client;
             const app = require("./app");
             const server = http.createServer(app);
-            let PORT = 3000;
+            let PORT = 5012;
             server.listen(PORT, function() {
                 console.log(`
                     The server is running on port: 
